95c9cb77d788802216524774de7922b24bd0fb89
Michel Stroobants <michel.stroobants@lavetan.be>
2023-03-08 12:01:10 +0100

    Implemented theming

--- App.js
+++ App.js
@@ -1,42 +1,112 @@
-import React from 'react';
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import ThemeManager, { themable } from 'react-themable';
-import Button from './src/components/Button';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  Provider as PaperProvider,
+  useTheme,
+  MD3LightTheme,
+  MD3DarkTheme,
+  MD3Theme,
+  adaptNavigationTheme,
+  ToggleButton,
+} from 'react-native-paper';
+import { useKeepAwake } from 'expo-keep-awake';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
 
-// tip: import theme variables from different files (e.g. ./themes/dark.theme.js)
-const globalThemeVariables = { fontSize: 20 };
-const darkThemeVariables = { backgroundColor: '#333333', color: '#666666' };
-const lightThemeVariables = { backgroundColor: '#eeeeee', color: '#111111' };
+const PERSISTENCE_KEY = 'NAVIGATION_STATE';
+const PREFERENCES_KEY = 'APP_PREFERENCES';
 
-// configure variables (optional, but makes life easier)
-// if I dont pass a theme, styles will be applied globally.
-// you can also specify the themes as an array at the first argument (e.g. ['dark', 'light'])
-ThemeManager.addVariables(globalThemeVariables);
-ThemeManager.addVariables('dark', darkThemeVariables);
-ThemeManager.addVariables('light', lightThemeVariables);
-
-const ThemableView = themable(View);
+export const PreferencesContext = React.createContext(null);
 
 export default function App() {
-  const { styles } = ThemeManager;
+  useKeepAwake();
+
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+  const [themeVersion, setThemeVersion] = React.useState(3);
+
+  const themeMode = isDarkMode ? 'dark' : 'light';
+
+  // const theme = {
+  //   3: {
+  //     light: MD3LightTheme,
+  //     dark: MD3DarkTheme,
+  //   }[themeVersion][themeMode],
+  // };
+
+  // const preferences = React.useMemo(
+  //   () => ({
+  //     toggleTheme: () => setIsDarkMode((oldValue) => !oldValue),
+  //     toggleCustomFont: () => setCustomFont(!customFontLoaded),
+  //     toggleThemeVersion: () => {
+  //       setCustomFont(false);
+  //       setCollapsed(false);
+  //       setThemeVersion((oldThemeVersion) => (oldThemeVersion === 2 ? 3 : 2));
+  //     },
+  //     customFontLoaded,
+  //     collapsed,
+  //     rtl,
+  //     theme,
+  //   }),
+  //   [rtl, theme, collapsed, customFontLoaded]
+  // );
+
+  const { LightTheme, DarkTheme } = adaptNavigationTheme({
+    reactNavigationLight: NavigationDefaultTheme,
+    reactNavigationDark: NavigationDarkTheme,
+  });
+
+  const CombinedDefaultTheme = {
+    ...MD3LightTheme,
+    ...LightTheme,
+    colors: {
+      ...MD3LightTheme.colors,
+      ...LightTheme.colors,
+    },
+  };
+
+  const CombinedDarkTheme = {
+    ...MD3DarkTheme,
+    ...DarkTheme,
+    colors: {
+      ...MD3DarkTheme.colors,
+      ...DarkTheme.colors,
+    },
+  };
+
+  const combinedTheme = isDarkMode ? CombinedDarkTheme : CombinedDefaultTheme;
 
   return (
-    <ThemableView style={styles.container} theme="blue">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button>Blue (Getting from context)</Button>
-      <Button theme="dark">dark</Button>
-      <Button theme="light">light</Button>
-    </ThemableView>
+    <PaperProvider theme={combinedTheme}>
+      <View
+        style={[
+          styles.container,
+          { backgroundColor: combinedTheme.colors.background },
+        ]}
+      >
+        <Text style={{ color: combinedTheme.colors.primary }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+        <ToggleButton.Row
+          value="left"
+          onValueChange={() => setIsDarkMode((oldValue) => !oldValue)}
+        >
+          <ToggleButton icon="flashlight" value="left" />
+          <ToggleButton icon="flashlight-off" value="right" />
+        </ToggleButton.Row>
+      </View>
+    </PaperProvider>
   );
 }
 
-ThemeManager.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     color: '#fff',
-    backgroundColor: 'black',
+    //backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
