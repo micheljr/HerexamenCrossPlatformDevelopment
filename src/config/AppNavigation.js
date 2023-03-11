e2793133e22e87a02417ddf8907fa185a840b165
Michel Stroobants <mistroobants@domain.local>
2023-03-11 01:31:44 +0100

    Added Navigation Header

--- src/config/AppNavigation.js
+++ src/config/AppNavigation.js
@@ -1,15 +1,22 @@
-import React from 'react-native';
+import React, { Platform, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 import { NavigationContainer, useTheme } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import { getHeaderTitle } from '@react-navigation/elements';
+import { Appbar } from 'react-native-paper';
+
 import Home from '../screens/Home';
 import DrawerItems from '../components/DrawerItems';
 
 const PERSISTENCE_KEY = 'NAVIGATION_STATE';
 
-const RootStack = createNativeStackNavigator();
+const RootStack = createStackNavigator();
 
 export default function RootNavigation({ toggleTheme }) {
   return (
@@ -55,34 +62,61 @@ export function DrawerNavigation({ toggleTheme }) {
               drawerContent={() => <DrawerContent toggleTheme={toggleTheme} />}
             >
               {/* </Drawer.Navigator><Drawer.Navigator> */}
-              <Drawer.Screen name="Home" options={{ headerShown: false }}>
+              <Drawer.Screen name="Home">
                 {() => <HomeNavigator toggleTheme={toggleTheme} />}
               </Drawer.Screen>
             </Drawer.Navigator>
           );
         }}
       </SafeAreaInsetsContext.Consumer>
+      <StatusBar style={!theme.isV3 || theme.dark ? 'light' : 'dark'} />
     </NavigationContainer>
   );
 }
 
-const DrawerContent = ({ toggleTheme }) => {
-  const theme = useTheme();
+const DrawerContent = ({ toggleTheme }) => (
+  // <PreferencesContext.Consumer>
+  //   {(preferences) => (
+  <DrawerItems toggleTheme={toggleTheme} />
+  //   )}
+  // </PreferencesContext.Consumer>
+);
 
-  return (
-    // <PreferencesContext.Consumer>
-    //   {(preferences) => (
-    <DrawerItems toggleTheme={toggleTheme} />
-    //   )}
-    // </PreferencesContext.Consumer>
-  );
-};
-
-const HomeScreen = createNativeStackNavigator();
+const HomeScreen = createStackNavigator();
 
 export function HomeNavigator({ toggleTheme }) {
+  const theme = useTheme();
+  const cardStyleInterpolator =
+    Platform.OS === 'android'
+      ? CardStyleInterpolators.forFadeFromBottomAndroid
+      : CardStyleInterpolators.forHorizontalIOS;
+
   return (
-    <HomeScreen.Navigator>
+    <HomeScreen.Navigator
+      screenOptions={({ navigation }) => {
+        return {
+          detachPreviousScreen: !navigation.isFocused(),
+          cardStyleInterpolator,
+          header: ({ route, options, back }) => {
+            const title = getHeaderTitle(options, route.name);
+            return (
+              <Appbar.Header elevated>
+                {back ? (
+                  <Appbar.BackAction onPress={() => navigation.goBack()} />
+                ) : navigation.openDrawer() ? (
+                  <Appbar.Action
+                    icon="menu"
+                    isLeading
+                    onPress={() => navigation.openDrawer()}
+                  />
+                ) : null}
+                <Appbar.Content title={title} />
+              </Appbar.Header>
+            );
+          },
+        };
+      }}
+    >
       <HomeScreen.Screen
         name="Main"
         //   component={Home}
